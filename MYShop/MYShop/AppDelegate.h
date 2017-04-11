fbc83c3dd72fe119c882bccd6311561fe0a24f15
hyp <hyp@HYP.local>
2017-04-11 15:21:20 +0800

    初始化项目

---
+++ MYShop/MYShop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MYShop
+//
+//  Created by hyp on 2017/4/11.
+//  Copyright © 2017年 youfenghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MYShop/MYShop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MYShop
+//
+//  Created by hyp on 2017/4/11.
+//  Copyright © 2017年 youfenghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
